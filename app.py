381aa05ce48888533e5d841f0ada03fb36eeaa2a
SingAvi <desci.avinash@gmail.com>
2019-07-07 02:53:11 +0530

    class Instagram:   def detail   def login   def search

---
+++ app.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import *
+import time
+
+class Instagram:
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+        self.bot = webdriver.Chrome()
+
+
+    def login(self):
+        bot = self.bot
+        bot.get('https://www.instagram.com/accounts/login/?source=auth_switcher/')
+        time.sleep(3)
+        email = bot.find_element_by_name('username')
+        password = bot.find_element_by_name('password')
+        email.clear()
+        password.clear()
+        email.send_keys(self.username)
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+
+
+
+
+
+ob = Instagram('singavi__','singavi__1411')
+ob.login()
